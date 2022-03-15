b680ad5932211ce4e1d509f915507e4ba0f5dfb9
smnmnt <o.kosobreev@yandex.ru>
2022-03-15 12:47:59 +0300

    gsdfs

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,9 +1,15 @@
-let title = 0
-let screens = 0
-let screenPrice = 0
-let rollback = 0
-let fullPrice = 0
-let adaptive = 0
+let title = "JavaScript"
+let screens = "Простые, Сложные, Интерактивные"
+let screenPrice = 12000
+let rollback = 30
+let fullPrice = 200000
+let adaptive = true
+let lowerCaseScreens = (screens.toLowerCase())
 
-alert("ZZZZZZZ")
-console.log("consolelog chuchc")
+console.log(title, fullPrice, adaptive)
+console.log(screens.length)
+console.log("Стоимость верстки экранов", screenPrice, "рублей и",
+ "Стоимость разработки сайта", fullPrice, "рублей")
+
+console.log(lowerCaseScreens.split(", "))
+console.log(fullPrice * (rollback/100))
